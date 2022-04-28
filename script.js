e378beb3e50b7a4bed39549e517502de00e87f3e
mkreive <mkreive@gmail.com>
2022-04-28 11:55:32 +0300

    redoing

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 "use strict";
-// ONLOAD FUNCTIONS
-// const daysLeftCounter = function (startDay, endDay) {
-//     let currentDay = new Date();
-//     let end = new Date(endDay);
-//     let start = new Date(startDay);
-//     const oneDay = 24 * 60 * 60 * 1000;
-//     console.log(currentDay);
-//     console.log(startDay);
-//     // return Math.ceil((end.getTime() - currentDay.getTime()) / oneDay);
-// };
 
 // DATA
 let pledges = [
     {
         id: 1,
         name: "Mastercraft Bamboo Monitor Riser",
+        about: "A beautiful & handcrafted monitor stand to reduce neck and eye strain.",
         rewards: [
             {
                 id: 1,
@@ -49,6 +40,7 @@ let pledges = [
     {
         id: 2,
         name: "Magnificent New Board Game Project",
+        about: "Neverseen before board game you will want to play everyday",
         rewards: [
             {
                 id: 1,
@@ -96,6 +88,7 @@ const navLinks = document.querySelectorAll(".nav__link");
 const pledgeMOdalHeaders = modalPledge.querySelectorAll(".header-medium");
 const chosenProject = document.getElementById("project");
 const rewardHeaderEl = document.querySelectorAll(".reward-name");
+const aboutPledgeEl = document.querySelector(".about-pledge");
 
 // pledge info
 const pledgeSumGoalEl = document.getElementById("sum-goal");
@@ -103,6 +96,7 @@ const pledgeSumBackedEl = document.getElementById("sum-backed");
 const pledgeBackersCountEl = document.getElementById("backers");
 const pledgeDaysLeftEl = document.getElementById("days-left");
 const progressBar = document.getElementById("progress");
+const pledegeInputValue = document.querySelectorAll(".pledge-amount");
 
 // rewards card
 const pledgeInfoEl = document.querySelector(".pledge-main");
@@ -113,44 +107,33 @@ const rewardBiddingSumEl = document.querySelectorAll(".bidding");
 // VARIABLES
 let openedModal;
 let activePledge;
-let pledgeActive;
 let bookmarked;
-let chosenReward;
+let chosenRewardId;
+let pledgeActive;
 
 // FUNCTIONS
 // local storage
-const setLocalStorage = function () {
-    localStorage.setItem("pledge", JSON.stringify(pledges));
+const setLocalStorage = function (key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
 };
-const getLocalStorage = function () {
-    const data = JSON.parse(localStorage.getItem("pledge"));
-    if (!data) {
-        bookmarked = false;
-    } else {
-        pledges = data;
-    }
-    return data;
-};
-
-const savePledgingInfo = function (value, reward) {
-    const pledgeSum = value;
-    const pledgedRewardId = reward;
-    const pldegeId = activePledge.id;
-    let localStorageData = getLocalStorage();
-
-    if (!localStorageData) {
-        const [pledgeSelected] = pledges.filter(
-            (pledge) => pledge.id === pldegeId
-        );
-        const [rewardSelected] = pledgeSelected.rewards.filter(
-            (rew) => rew.id == pledgedRewardId
-        );
-        rewardSelected.countLeft--;
+const getLocalStorage = function (key) {
+    const data = JSON.parse(localStorage.getItem(key));
+    if (data) {
+        return data;
+    } else if (!data) {
+        return;
     }
 };
+const removeItemLocalStorage = function (key, value) {
+    if (!key || !value) return;
+    const data = getLocalStorage(key);
+    if (data == value) window.localStorage.removeItem(key);
+};
 
-// rendering elements
+// rendering HTML elements
 const innerTextSetter = function (pledge) {
+    chosenProject.innerText = `${pledge.name}`;
+    aboutPledgeEl.innerText = `${pledge.about}`;
     pledgeSumGoalEl.innerText = `of $${pledge.sumGoal.toLocaleString()} backed`;
     pledgeSumBackedEl.innerText = `$${pledge.sumBacked.toLocaleString()}`;
     pledgeBackersCountEl.innerText = pledge.numBackers.toLocaleString();
@@ -169,35 +152,31 @@ const renderReward = function (pledge) {
                 ? "card card-pledge"
                 : "card card-pledge inactive"
         }" >
-        <div class="couples">
-            <h3 class="header-medium reward-name" id="${reward.id}">
-                ${reward.name}
-            </h3>
-            <span class="bidding">Pledge $${reward.minSum} or more</span>
-        </div>
-        <p class="text">
-            You get a Black Special Edition computer stand
-            and a personal thank you. You’ll be added to our
-            Backer member list. Shipping is included.
-        </p>
-        <div class="couples">
-            <div class="couples-left">
-                <span class="header-big rewards-count"
-                    >${reward.countLeft}</span
-                ><span class="text">left</span>
+            <div class="couples">
+                <h3 class="header-medium reward-name" id="${reward.id}">
+                ${reward.name}</h3>
+                <span class="bidding">Pledge $${reward.minSum} or more</span>
             </div>
-            <button class="btn reward">
-                ${reward.countLeft > 0 ? "Select Reward" : "Out of Stock"}
-            </button>
-        </div>
-    </div>`;
-
+            <p class="text">
+                You get a Black Special Edition computer stand
+                and a personal thank you. You’ll be added to our
+                Backer member list. Shipping is included.
+            </p>
+            <div class="couples">
+                <div class="couples-left">
+                    <span class="header-big rewards-count"
+                        >${reward.countLeft}</span
+                    ><span class="text">left</span>
+                </div>
+                <button class="btn reward">
+                    ${reward.countLeft > 0 ? "Select Reward" : "Out of Stock"}
+                </button>
+            </div>
+        </div>`;
         pledgeInfoEl.insertAdjacentHTML("beforeend", html);
     });
-
-    selectRewarListener();
+    activateRewardListeners();
 };
-
 const renderRewardsModal = function (pledge) {
     const rewards = pledge.rewards;
     let i = 1;
@@ -209,43 +188,39 @@ const renderRewardsModal = function (pledge) {
                     ? "card card-modal"
                     : "card card-modal inactive"
             }" id="${i++}">
-            <div>
-                <input type="radio" name="radio" class="card-radio" />
-            </div>
-            <div>
-                <div class="couples">
-                    <div class="couples-left">
-                        <h3 class="header-medium">${reward.name}</h3>
-                        <span class="bidding">Pledge $${
-                            reward.minSum
-                        } or more</span>
+                <div>
+                    <input type="radio" name="radio" class="card-radio" />
+                </div>
+                <div>
+                    <div class="couples">
+                        <div class="couples-left">
+                            <h3 class="header-medium">${reward.name}</h3>
+                            <span class="bidding">Pledge $${
+                                reward.minSum
+                            } or more</span>
                     </div>
                     <div class="couples-left">
                         <span class="header-medium">${reward.countLeft}</span
                         ><span class="text">left</span>
                     </div>
                 </div>
-            <p class="text">
-                You get an ergonomic stand made of natural bamboo.
-                You've helped us launch our promotional campaign,
-                and you’ll be added to a special Backer member list.
-            </p>
-        </div>
-    </div>
-    `;
-
+                <p class="text">
+                    You get an ergonomic stand made of natural bamboo.
+                    You've helped us launch our promotional campaign,
+                    and you’ll be added to a special Backer member list.
+                </p>
+            </div>
+        </div>`;
         modalPledge.insertAdjacentHTML("beforeend", html);
     });
-
     rewardModalListener(rewards);
 };
 
 // UI stuff
 const openModal = function (modal) {
     openedModal = modal;
-    modal.classList.remove("hidden");
+    openedModal.classList.remove("hidden");
     overlay.classList.remove("hidden");
-    renderRewardsModal(activePledge);
     scroll(0, 0);
 };
 const closeModal = function (modal) {
@@ -254,108 +229,115 @@ const closeModal = function (modal) {
 };
 
 const selectPledge = function (pledge) {
-    pledgeActive = pledge;
-    pledge.classList.add("active");
-    pledgeAddonElement.classList.remove("hidden");
-    pledge.appendChild(pledgeAddonElement);
+    if (!pledgeActive) {
+        pledgeActive = pledge;
+        pledge.classList.add("active");
+        pledgeAddonElement.classList.remove("hidden");
+        pledge.appendChild(pledgeAddonElement);
+    } else {
+        activePledgeRemoving(pledgeActive);
+        pledgeActive = pledge;
+        pledge.classList.add("active");
+        pledgeAddonElement.classList.remove("hidden");
+        pledge.appendChild(pledgeAddonElement);
+    }
 };
-const activePledgeRemoving = function () {
-    let oldActivePledge = pledgeActive;
+const activePledgeRemoving = function (pledge) {
+    let oldActivePledge = pledge;
     oldActivePledge.classList.remove("active");
     oldActivePledge.removeChild(pledgeAddonElement);
 };
-
 const bookmarkIt = function () {
     bookmarkBtn.classList.add("bookmarked");
     bookmarkBtn.innerText = "Bookmarked!";
-    bookmarked = true;
 };
 const removeBookmark = function () {
     bookmarkBtn.classList.remove("bookmarked");
     bookmarkBtn.innerText = "Bookmark";
-    bookmarked = false;
+};
+
+// saving pledging info
+const savePledgingInfo = function (value, reward) {
+    const pledgeSum = value;
+    const pledgedRewardId = reward;
+    const pldegeId = activePledge.id;
+    let localStorageData = getLocalStorage();
+
+    if (!localStorageData) {
+        const [pledgeSelected] = pledges.filter(
+            (pledge) => pledge.id === pldegeId
+        );
+        const [rewardSelected] = pledgeSelected.rewards.filter(
+            (rew) => rew.id == pledgedRewardId
+        );
+        rewardSelected.countLeft--;
+    }
 };
 
 // EVENT HANDLERS
 // page load
 window.addEventListener("load", function () {
-    const savedData = getLocalStorage();
+    // getting local storage data
+    const pledgeDataStorage = getLocalStorage("pledge");
+    const bookmarksDataStorage = getLocalStorage("bookmarks");
 
-    if (!savedData) {
+    // setting active pledge
+    if (pledgeDataStorage) {
+        [activePledge] = pledgeDataStorage;
+    } else if (bookmarksDataStorage) {
+        bookmarked = bookmarksDataStorage;
+        activePledge = pledges.find((pledge) => pledge.id == bookmarked);
+        bookmarkIt();
+    } else if (!pledgeDataStorage && !bookmarksDataStorage) {
         activePledge = pledges[0];
-    } else if (savedData) {
-        const [...bookmarkedPledges] = savedData.filter(
-            (pledge) => pledge.bookmarked === true
-        );
-
-        if (bookmarkedPledges.length < 1) {
-            activePledge = pledges[0];
-        } else {
-            bookmarkIt();
-            activePledge = bookmarkedPledges[0];
-        }
-        if (!activePledge) {
-            activePledge = pledges[0];
-        }
     }
 
+    // rendering page
     innerTextSetter(activePledge);
     renderReward(activePledge);
 });
 
-// buttons listeners
+// bookmarking
 bookmarkBtn.addEventListener("click", function () {
-    if (activePledge.bookmarked === false) {
-        localStorage.clear();
+    const bookmarkedPledges = getLocalStorage("bookmarks");
+
+    if (!bookmarkedPledges) {
         activePledge.bookmarked = true;
-        setLocalStorage();
+        setLocalStorage("bookmarks", activePledge.id);
         bookmarkIt();
-    } else if (activePledge.bookmarked === true) {
-        localStorage.clear();
+    } else {
         activePledge.bookmarked = false;
-        setLocalStorage();
         removeBookmark();
+        const bookmarkId = activePledge.id;
+        removeItemLocalStorage("bookmarks", bookmarkId);
     }
 });
 
-backProjectBtn.addEventListener("click", function () {
-    openModal(modalPledge);
-});
-
-closeBtn.addEventListener("click", function () {
-    closeModal(modalPledge);
-});
-
-const selectRewarListener = function () {
+// home page reward button listeners
+const activateRewardListeners = function () {
     const selectRewardButton = document.querySelectorAll(".reward");
     selectRewardButton.forEach((rewardBtn) => {
-        rewardBtn.addEventListener("click", function (e) {
-            let parentNode = e.target.parentNode.parentNode;
-            let rewardName =
-                parentNode.querySelector(".header-medium").innerText;
-
-            pledgeMOdalHeaders.forEach((header) => {
-                if (header.innerText.trim("") == rewardName) {
-                    const activeCard = header.closest(".card-modal");
-                    selectPledge(activeCard);
-                } else {
-                    return;
-                }
-            });
+        rewardBtn.addEventListener("click", function () {
             openModal(modalPledge);
+            renderRewardsModal(activePledge);
         });
     });
 };
-// modal listeners
+backProjectBtn.addEventListener("click", function () {
+    openModal(modalPledge);
+    renderRewardsModal(activePledge);
+});
+
+// rewards modal listener
 const rewardModalListener = function (rewards) {
     const radioButtonEl = document.querySelectorAll(".card-radio");
     const rewardSubmitBtn = document.querySelectorAll(".submit-pledge");
 
     radioButtonEl.forEach((radio) => {
         radio.addEventListener("click", function (e) {
-            const parentNode = e.target.parentNode.parentNode;
-            chosenReward = parentNode.id;
+            parentNode = e.target.parentNode.parentNode;
             selectPledge(parentNode);
+            chosenRewardId = parentNode;
         });
     });
 
@@ -365,7 +347,7 @@ const rewardModalListener = function (rewards) {
             if (inputValue > 0 && inputValue) {
                 closeModal(modalPledge);
                 openModal(successModal);
-                savePledgingInfo(inputValue, chosenReward);
+                savePledgingInfo(inputValue, chosenRewardId);
             } else return;
         });
     });
@@ -375,3 +357,6 @@ const rewardModalListener = function (rewards) {
         closeModal(successModal);
     });
 };
+closeBtn.addEventListener("click", function () {
+    closeModal(modalPledge);
+});
